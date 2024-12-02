8e7f07b18ce6091f4188c1865fe4311f74d3a00d
VisualNL <r.m.wagt@gmail.com>
2024-12-02 22:57:13 +0100

    day-2 functies gemaakt

--- day-2/day-2.ps1
+++ day-2/day-2.ps1
@@ -1,3 +1,39 @@
-$array = Get-Content -path "day-2-input.txt"
+function Is-Increasing {
+    param (
+       [int[]]$array 
+    )
+    for($i = 1 ; $i -lt $array.length; $i++) {
+        if($array[$i] -le $array[$i - 1]) {
+           return $false
+        }
+    }
+    return $true
+}
 
-write-host $array[0]
+function Is-Decreasing {
+    param (
+       [int[]]$array 
+    )
+    for($i = 1 ; $i -lt $array.length; $i++) {
+        if($array[$i] -ge $array[$i - 1]) {
+           return $false
+        }
+    }
+    return $true
+}
+
+
+
+$array = Get-Content -path "day-2-input.txt"
+$levels = @{}
+$index = 1
+
+foreach($level in $array) {
+    $levels[$index] = $level.Split(' ') | ForEach-Object { [int]$_ }
+    $index++
+}
+
+
+# Toon de uitkomst van de controle
+Write-Host "Level 1 is increasing: $(Is-Increasing -array $levels[1])"
+Write-Host "Level 1 is decreasing: $(Is-Decreasing -array $levels[923])"
