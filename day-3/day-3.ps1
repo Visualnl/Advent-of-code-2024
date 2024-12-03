31d57b0ec35229c8347d16747546e0d419391d5d
VisualNL <r.m.wagt@gmail.com>
2024-12-03 21:35:30 +0100

    Dag 3 klaar

--- day-3/day-3.ps1
+++ day-3/day-3.ps1
@@ -1,19 +1,20 @@
-#dag 3
+
+$verwijderdPattern = "(?s)don't.*?(?=do)"
+
 
 $string = Get-Content "day-3-input.txt" -Raw
-
+$string = $string -replace $verwijderdPattern, ''
 
 
 $pattern = 'mul\((\b\d+),(\b\d+)\)'
 
+$resultaten = [regex]::Matches($string, $pattern)
 
-$resultaten = [regex]::Matches($string,$pattern)
-
-$opgeteld  = 0
-foreach($resultaat in $resultaten){
-    $som = [int] $resultaat.Groups[1].value * [int] $resultaat.Groups[2].value
+$opgeteld = 0
+foreach ($resultaat in $resultaten) {
+    $som = [int]$resultaat.Groups[1].Value * [int]$resultaat.Groups[2].Value
     $opgeteld += $som
- 
 }
 
-Write-Host $opgeteld
+
+Write-Host "De totale som is: $opgeteld"
