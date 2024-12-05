447497cf73dd501b8ce6bc44e5cb6fdac08410fb
VisualNL <r.m.wagt@gmail.com>
2024-12-05 22:24:30 +0100

    part1 dag5 opgelost

--- day-5/day-5.ps1
+++ day-5/day-5.ps1
@@ -1,1 +1,49 @@
-Write-host "test world"
+# Stap 1: Lees de inhoud van het bestand
+$input = Get-Content "day-5-input.txt"
+
+
+$splitInput= $input.indexOf('')
+$regels = $input[0..$splitInput]
+$updates = $input[($splitInput+1)..$input.Length]
+
+
+$regelsGesplitst = $regels | ForEach-Object {
+    $splitsingOp = $_ -split '\|'
+    [PSCustomObject]@{
+        eersteGetal = [int]$splitsingOp[0]
+        tweedeGetal = [int]$splitsingOp[1]
+    }
+}
+
+$somMidden = 0
+
+foreach ($update in $updates) {
+    $updateArray = $update -split ',' | ForEach-Object {
+        [int] $_
+    }
+    $correct = $true 
+    foreach($regel in $regelsGesplitst) {
+        $eerste = $regel.eersteGetal
+        $tweede =$regel.tweedeGetal
+       
+        if($updateArray -contains $eerste -and $updateArray -contains $tweede){
+            $eersteIndex = $updateArray.indexOf($eerste)
+            $tweedeIndex = $updateArray.indexOf($tweede)
+
+            if($eersteIndex -gt $tweedeIndex) {
+                $correct = $false
+                break
+            }
+        }
+    }
+    if($correct) {
+       $midden = [math]::Floor($updateArray.Length / 2)
+       $middenGetal = $updateArray[$midden]
+       $somMidden += $middenGetal
+    }
+
+}
+
+
+
+Write-Host $somMidden
