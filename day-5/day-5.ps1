67cde5ec167e6a78fdb02bb2aa8b304fb911d114
VisualNL <r.m.wagt@gmail.com>
2024-12-05 22:47:35 +0100

    dag 5 finished

--- day-5/day-5.ps1
+++ day-5/day-5.ps1
@@ -1,10 +1,8 @@
-# Stap 1: Lees de inhoud van het bestand
 $input = Get-Content "day-5-input.txt"
 
-
-$splitInput= $input.indexOf('')
-$regels = $input[0..$splitInput]
-$updates = $input[($splitInput+1)..$input.Length]
+$splitInput = $input.IndexOf('')
+$regels = $input[0..($splitInput - 1)]
+$updates = $input[($splitInput + 1)..$input.Length]
 
 
 $regelsGesplitst = $regels | ForEach-Object {
@@ -16,34 +14,68 @@ $regelsGesplitst = $regels | ForEach-Object {
 }
 
 $somMidden = 0
+$verkeerdeUpdates = @()
+$somMiddenGecorrigeerd = 0
+
 
 foreach ($update in $updates) {
-    $updateArray = $update -split ',' | ForEach-Object {
-        [int] $_
-    }
+    $updateArray = $update -split ',' | ForEach-Object { [int]$_ }
     $correct = $true 
-    foreach($regel in $regelsGesplitst) {
-        $eerste = $regel.eersteGetal
-        $tweede =$regel.tweedeGetal
-       
-        if($updateArray -contains $eerste -and $updateArray -contains $tweede){
-            $eersteIndex = $updateArray.indexOf($eerste)
-            $tweedeIndex = $updateArray.indexOf($tweede)
 
-            if($eersteIndex -gt $tweedeIndex) {
+    foreach ($regel in $regelsGesplitst) {
+        $eerste = $regel.eersteGetal
+        $tweede = $regel.tweedeGetal
+       
+        if ($updateArray -contains $eerste -and $updateArray -contains $tweede) {
+            $eersteIndex = $updateArray.IndexOf($eerste)
+            $tweedeIndex = $updateArray.IndexOf($tweede)
+
+            if ($eersteIndex -gt $tweedeIndex) {
                 $correct = $false
+                $verkeerdeUpdates += ,@($updateArray)
                 break
             }
         }
     }
-    if($correct) {
-       $midden = [math]::Floor($updateArray.Length / 2)
-       $middenGetal = $updateArray[$midden]
-       $somMidden += $middenGetal
-    }
 
+    if ($correct) {
+       
+        $midden = [math]::Floor($updateArray.Length / 2)
+        $middenGetal = $updateArray[$midden]
+        $somMidden += $middenGetal
+        
+    } else {
+       
+        $tempUpdateArray = $updateArray
+
+        $correct = $false
+        while (!$correct) {
+            $correct = $true
+            foreach ($regel in $regelsGesplitst) {
+                $eerste = $regel.eersteGetal
+                $tweede = $regel.tweedeGetal
+               
+                if ($tempUpdateArray -contains $eerste -and $tempUpdateArray -contains $tweede) {
+                    $eersteIndex = $tempUpdateArray.IndexOf($eerste)
+                    $tweedeIndex = $tempUpdateArray.IndexOf($tweede)
+
+                    if ($eersteIndex -gt $tweedeIndex) {
+                        
+                        $tempUpdateArray[$eersteIndex], $tempUpdateArray[$tweedeIndex] = $tempUpdateArray[$tweedeIndex], $tempUpdateArray[$eersteIndex]
+                        $correct = $false  
+                    }
+                }
+            }
+        }
+
+      
+        $middenGecorrigeerd = [math]::Floor($tempUpdateArray.Length / 2)
+        $middenGetalGecorrigeerd = $tempUpdateArray[$middenGecorrigeerd]
+        $somMiddenGecorrigeerd += $middenGetalGecorrigeerd
+         
+    }
 }
 
 
-
-Write-Host $somMidden
+Write-Host "Som van de middenpagina's voor correcte updates: $somMidden"
+Write-Host "Som van de middenpagina's voor gecorrigeerde updates: $somMiddenGecorrigeerd"
